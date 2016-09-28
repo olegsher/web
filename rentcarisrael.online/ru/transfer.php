938750fb20a29cad6213557b4a7e4fce00359258
olegsher <github@sher.biz>
2016-09-28 12:51:44 +0300

    rentcarisrael.online added yandex counter

--- rentcarisrael.online/ru/transfer.php
+++ rentcarisrael.online/ru/transfer.php
@@ -1,26 +1,47 @@
-<?php
-/* 
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-
-/**
- * @author root
- */
-
-// DO NOT REMOVE "%INCLUDE_PATH%" FROM TEMPLATE!
-// TODO: check include path
-ini_set('include_path', ini_get('include_path')%INCLUDE_PATH%);
-
-// put your code here
 <!DOCTYPE HTML>
 <html>
 <head>
-</head>
+<title>Недорогая аренда прокат автомобиля в Изр</title>
+<?php include("_header.html"); ?>
+</header>
 <body>
-test
-include("_counters.html"); 
+<?php include("_counters.html"); ?>
+<!-- top-header -->
+<div class="top-header">
+	<div class="container">
+<!--		<ul class="tp-hd-lft wow fadeInLeft animated" data-wow-delay=".5s">
+			<li class="hm"><a href="index.html"><i class="fa fa-home"></i></a></li>
+			<li class="prnt"><a href="javascript:window.print()">Print/SMS Ticket</a></li>
+				
+		</ul>-->
+		<ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 
+			<li class="tol">Телефон : +972-58-7710101</li>				
+<!--			<li class="sig"><a href="#" data-toggle="modal" data-target="#myModal" >Sign Up</a></li> 
+			<li class="sigi"><a href="#" data-toggle="modal" data-target="#myModal4" >/ Sign In</a></li>-->
+        </ul>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--- /top-header ---->
+<!--- header ---->
+<div class="header">
+	<div class="container">
+		<div class="logo wow fadeInDown animated" data-wow-delay=".5s">
+			<a href="index.html">Vastama <span>Israel</span></a>	
+		</div>
+<!--		<div class="bus wow fadeInUp animated" data-wow-delay=".5s">
+            <a href="index.html" class="buses active">BUSES</a>
+            <a href="hotels.html">HOTELS</a>
+        </div>-->
+		<div class="lock fadeInDown animated" data-wow-delay=".5s"> 
+<!--			<li><i class="fa fa-lock"></i></li>-->
+            <li><div class="securetxt">ДЕШЕВО &amp; БЫСТРО<br> УДОБНО</div></li>
+			<div class="clearfix"></div>
+		</div>
+		<div class="clearfix"></div>
+	</div>
+</div>
+<!--- /header ---->
+
 </body>
 
-?>
