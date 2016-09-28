fc8ce0bf940069fbc00dfd2674f8c9017d9843ff
olegsher <github@sher.biz>
2016-09-28 13:40:07 +0300

    rentcarisrael.online added yandex counter

--- rentcarisrael.online/ru/transfer.php
+++ rentcarisrael.online/ru/transfer.php
@@ -2,11 +2,17 @@
 <html>
 <head>
 <title>Недорогая аренда прокат автомобиля в Изр</title>
+<meta name="keywords" content="машина прокат, машина прокат Израиле, аренда машины Израиль, avto prokat, prokat mashin, auto arenda, прокат аренда машин, где можно взять машину напрокат, заказать машину, аренда иномарок, arenda avto, стоимость аренды машины, израиль прокат автомобилей, съем машины Израиле, аренда авто, прокат машины, сьем автомобиля,автомобиль,  машина Израиле, дешевый прокат автомобиля" />
+<meta  name="description"  content="Аренда прокат автомобиля в Израиле. Бюджетные авто, Миниваны, "/>
+<meta property="og:title" content="Аренда прокат автомобилей и минивэнов в Израиле по низким ценам от компании Vastama Ltd тел +972-58-7710101" />
+<meta property="og:type" content="website" />
+
 <?php include("_header.html"); ?>
 </header>
 <body>
 <?php include("_counters.html"); ?>
 <?php include("_top.html"); ?>
+<?php include("transfer.html"); ?>
 
 <?php include("_footer.html"); ?>
 
