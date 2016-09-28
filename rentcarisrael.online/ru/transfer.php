754d10e7ca427d23b6f04db073ac15bad1d5b5a6
olegsher <github@sher.biz>
2016-09-28 13:10:50 +0300

    rentcarisrael.online added yandex counter

--- rentcarisrael.online/ru/transfer.php
+++ rentcarisrael.online/ru/transfer.php
@@ -6,42 +6,9 @@
 </header>
 <body>
 <?php include("_counters.html"); ?>
-<!-- top-header -->
-<div class="top-header">
-	<div class="container">
-<!--		<ul class="tp-hd-lft wow fadeInLeft animated" data-wow-delay=".5s">
-			<li class="hm"><a href="index.html"><i class="fa fa-home"></i></a></li>
-			<li class="prnt"><a href="javascript:window.print()">Print/SMS Ticket</a></li>
-				
-		</ul>-->
-		<ul class="tp-hd-rgt wow fadeInRight animated" data-wow-delay=".5s"> 
-			<li class="tol">Телефон : +972-58-7710101</li>				
-<!--			<li class="sig"><a href="#" data-toggle="modal" data-target="#myModal" >Sign Up</a></li> 
-			<li class="sigi"><a href="#" data-toggle="modal" data-target="#myModal4" >/ Sign In</a></li>-->
-        </ul>
-		<div class="clearfix"></div>
-	</div>
-</div>
-<!--- /top-header ---->
-<!--- header ---->
-<div class="header">
-	<div class="container">
-		<div class="logo wow fadeInDown animated" data-wow-delay=".5s">
-			<a href="index.html">Vastama <span>Israel</span></a>	
-		</div>
-<!--		<div class="bus wow fadeInUp animated" data-wow-delay=".5s">
-            <a href="index.html" class="buses active">BUSES</a>
-            <a href="hotels.html">HOTELS</a>
-        </div>-->
-		<div class="lock fadeInDown animated" data-wow-delay=".5s"> 
-<!--			<li><i class="fa fa-lock"></i></li>-->
-            <li><div class="securetxt">ДЕШЕВО &amp; БЫСТРО<br> УДОБНО</div></li>
-			<div class="clearfix"></div>
-		</div>
-		<div class="clearfix"></div>
-	</div>
-</div>
-<!--- /header ---->
+<?php include("_top.html"); ?>
+
+<?php include("_footer.html"); ?>
 
 </body>
 
