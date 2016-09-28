8878d9e01d133b73b9831fbfae9b7b857d9d1b5f
olegsher <github@sher.biz>
2016-09-28 12:09:13 +0300

    rentcarisrael.online added yandex counter

---
+++ rentcarisrael.online/ru/transfer.php
@@ -0,0 +1,26 @@
+<?php
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ * @author root
+ */
+
+// DO NOT REMOVE "%INCLUDE_PATH%" FROM TEMPLATE!
+// TODO: check include path
+ini_set('include_path', ini_get('include_path')%INCLUDE_PATH%);
+
+// put your code here
+<!DOCTYPE HTML>
+<html>
+<head>
+</head>
+<body>
+test
+include("_counters.html"); 
+</body>
+
+?>
