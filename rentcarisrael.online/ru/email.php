90cf3a688a4990786ad41386dc0a9731c699e5b7
Oleg Sher <github@sher.biz>
2016-10-03 16:19:42 +0300

    rentcarisrael.online ru

--- rentcarisrael.online/ru/email.php
+++ rentcarisrael.online/ru/email.php
@@ -94,22 +94,7 @@ $message .= "</body></html>";
     }
 ?>
 
-<!DOCTYPE HTML>
-<html>
-<head>
-<title>Недорогая аренда прокат автомобиля в Израиле | RentCarIsrael.online</title>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<meta name="keywords" content="машина прокат, машина прокат Израиле, аренда машины Израиль, avto prokat, prokat mashin, auto arenda, прокат аренда машин, где можно взять машину напрокат, заказать машину, аренда иномарок, arenda avto, стоимость аренды машины, израиль прокат автомобилей, съем машины Израиле, аренда авто, прокат машины, сьем автомобиля,автомобиль,  машина Израиле, дешевый прокат автомобиля" />
-<meta  name="description"  content="Аренда прокат автомобиля в Израиле. Бюджетные авто, Миниваны, семейные машины, класс люкс и джипы звоните +972-58-7710101" />
-<meta property="og:title" content="Аренда прокат автомобилей и минивэнов в Израиле по низким ценам от компании Vastama Ltd тел +972-58-7710101" />
-<meta property="og:type" content="website" />
 
-<?php include("__header.html"); ?>
-</header>
-<body>
-<?php include("__counters.html"); ?>
-<?php include("__top.html"); ?>
 
 <div class="container">
 	<div class="col-md-5 bann-info1 wow fadeInLeft animated" data-wow-delay=".5s">
@@ -286,12 +271,4 @@ $message .= "</body></html>";
     Модели автомобилей представлены исключительно для примера и при выдаче компания имеет право предоставить автомобиль того же класса, но другой марки.
 	<div class="clearfix"></div>
         
-    
-    
-    
-    
-    
-    
-    
-<?php include("__footer.html"); ?>
-</body>
+ 
