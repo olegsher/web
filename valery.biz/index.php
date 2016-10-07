153905b1f86598308b4cfe28d7d1b0eeafbd9650
Oleg Sher <github@sher.biz>
2016-10-07 23:43:42 +0300

    rentcarisrael.online ru

---
+++ valery.biz/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Valery.biz</title>
+<meta name="keywords" content="" />
+<meta  name="description"  content="" />
+<?php include("__header.html"); ?>
+</header>
+<body>
+
+<?php include("_index.html"); ?>
+<?php include("__footer.html"); ?>
+</body>
+
