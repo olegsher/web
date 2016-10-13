d8cc8d3fd64f01cccf727bf6974da30f864e5daf
Oleg Sher <github@sher.biz>
2016-10-13 22:40:08 +0300

    rentcarisrael.online ru

---
+++ rentcarisrael.online/ru/_tours.php
@@ -0,0 +1,114 @@
+<div class="container">
+	<div class="col-md-5 bann-info1 wow fadeInLeft animated" data-wow-delay=".5s">
+<!--		<i class="fa fa-columns"></i>-->
+<i> <img src="images/Thrifty-Israel.jpg" alt="машина прокат, машина прокат Израиле, аренда машины Израиль" class="img-responsive" /></i>
+		<h3>ИНДИВИДУАЛЬНЫЕ ЭКСКУРСИИ ПО ИЗРАИЛЮ</h3>
+                <h4>
+                Лицензированные профессиональные гиды 
+                <hr /> День работы гида 250$ 
+                <hr /> Нет скрытых платежей 
+                <hr /> Скидки при заказе нескольких экскурсий
+                <hr /> Оплата напрямую гиду, нет предоплаты, нет платы за изменение или отмену заказа
+                </h4>
+	</div>
+	<div class="col-md-7 bann-info wow fadeInRight animated" data-wow-delay=".5s">
+		<h1>Заполните форму и лучший гид Израиля свяжется с вами для назначения экскурсии</h1>
+                <form id="contactform" action="" method="post">
+		<div class="ban-top">
+			<div class="bnr-left">
+				<label class="inputLabel">Выбирайте экскурсию</label>
+<!--				<input class="city" type="text" value="Enter a city" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter a city';}" required=>-->
+                                <!-- Car select start -->
+                        
+						 <select type="text" value="Выбирайте экскурсию" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Выбирайте экскурсию';}" name="tour-select" id="car-select" required = "required" autofocus>							
+							<option value="Иерусалим трех религий" >Иерусалим трех религий</option>
+							<option value="Иерусалим Христианский" >Иерусалим Христианский</option>
+							<option value="Иерусалим Иудейский" >Иерусалим Иудейский</option>
+							<option value="Иерусалим Мусульманский" >Иерусалим Мусульманский</option>
+							<option value="Иерусалим Обзорный; Яд Вашем" >Иерусалим Обзорный, Яд Вашем</option>
+							<option value="Вифлеем и Иерусалим Христианский" >Вифлеем и Иерусалим Христианский</option>							
+							<option value="Вифлеем и Православный Русский Иерусалим вкл. Горненский монастырь в Эйн-Кареме" >Вифлеем и Православный Русский Иерусалим вкл. Горненский монастырь в Эйн-Кареме</option>
+							<option value="Вифлеем и Русский Иерусалим с Русским Подворьем" >Вифлеем и Русский Иерусалим с Русским Подворьем</option>
+							<option value="Иерусалим Подземный" >Иерусалим Подземный</option>
+                                                        <option value="Мини-Израиль. Латрун. Тель-Авив Яффо. Алмазная Биржа «Мини-Израиль»" >Мини-Израиль. Латрун. Тель-Авив Яффо. Алмазная Биржа «Мини-Израиль»</option>
+							<option value="Галилея Христианская. Назарет Галилея" >Галилея Христианская. Назарет Галилея</option>
+							<option value="Иерихон. Каранталь. Монастыри Иудейской пустыни. Вади Кельт. Каср аль-Йехуд Иерихон" >Иерихон. Каранталь. Монастыри Иудейской пустыни. Вади Кельт. Каср аль-Йехуд Иерихон</option>
+							<option value="Кейсария, Хайфа, Акко" >Кейсария, Хайфа, Акко</option>
+							<option value="Подземный Акко залы крестоносцев, Бахаи в Хайфе" >Подземный Акко залы крестоносцев, Бахаи в Хайфе</option>
+                                                        <option value="Сталактитовые пещеры и монастыри в окрестностях Иерусалима" >Сталактитовые пещеры и монастыри в окрестностях Иерусалима</option>
+                                                        <option value="Тель-Авив Яффо, Алмазная Биржа" >Тель-Авив Яффо, Алмазная Биржа</option>
+                                                        <option value="Заповедник Эйн Геди и Мертвое море" >Заповедник "Эйн Геди" и Мертвое море</option>
+                                                        <option value="Крепость Массада и Мёртвое море" >Крепость Массада и Мёртвое море</option>
+							
+						</select>
+                             <br />
+                      </div>
+			<!-- Car select end -->
+                     <div class="clearfix"></div>
+                      <hr />  
+                    
+                        <div class="bnr-left">
+				<label class="inputLabel">Аренда</label>
+<!--				<input class="city" type="text" value="Отделение" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'Enter a city';}" required=>-->
+                                <textarea name="city" placeholder="Город отправления"></textarea> <br />
+                                <input name="pickup-date" type="date"  min="2016-10-02" required = "required">
+<!--                                <input class="date" name="pickup-date" id="datepicker" type="date" value="dd-mm-yyyy" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'dd-mm-yyyy';}" required = "required">-->
+                                <input type="time" name="pickup-time" id="pickup-time" placeholder="Время" value="hh:mm" onfocus="this.value = '';" onblur="if (this.value == '') {this.value = 'hh:mm';}">
+			
+			</div>
+				
+                    
+                        
+<!--				<div class="clearfix"></div>-->
+		</div>
+		<div class="ban-bottom">
+                    
+				<div class="clearfix"></div>
+				
+                       <section> 
+                           <hr />
+                        Ваше имя <input type="text" name="First_name" placeholder="на английском" required = "required">
+                        Фамилия <input type="text" name="Last_name" placeholder="на английском" required = "required"> <br />
+                        Почта <input type="email" name="email" placeholder="" required = "required"> <br />
+                        Телефон <input type="text" name="phone" placeholder="включая код страны" required = "required"> <br />
+                         
+                        </section>
+		</div>
+
+			
+                        <section>
+                            <hr />
+                        
+                        <textarea name="message" placeholder="Дополнительная информация, вопрос..."></textarea> <br />
+                        </section>
+<!--                        <div class="g-recaptcha" data-sitekey="6LcVRQgUAAAAALqre7PqN_m3eNet_RK1XrRoj6Hz"></div>-->
+                        <br />
+                        <input type="submit" name="submit" value="ЗАКАЗАТЬ">
+                        
+
+                        
+  </form>
+                
+ 
+
+<!--		</div>-->
+	</div>
+    <hr />
+    
+    
+    <div class="clearfix"></div>
+        
+<hr />
+<div class="col-md-7 bann-info wow fadeInRight animated" data-wow-delay=".5s">
+<img src="images/catJ.jpg" alt="машина прокат, машина прокат Израиле, аренда машины Израиль" class="img-responsive" />
+
+    <br /><h2>ЕСТЬ ВОПРОСЫ ПО ЭКСКУРСИИ?</h2>
+</div> 
+
+<br />Любые вопросы будут разрешены если вы свяжитесь с нами
+ <br /><img src="images/phone.gif" alt="Позвоните Vastama по аренде атомобиля в Израиле" /> Телефон в Израиле +972-58-7710101
+ <br /><img src="images/whatsapp.png" alt="Позвоните Vastama по аренде атомобиля в Израиле" /> WhatsApp +972-58-7710101
+ <br /><img src="images/viber.png" alt="Позвоните Vastama по аренде атомобиля в Израиле" /> Viber +972-58-7710101
+ <br /><img src="images/mail.gif" alt="Позвоните Vastama по аренде атомобиля в Израиле" /> Почта rentcar@vastama.com
+ <br />говорим по русски
+</div>
