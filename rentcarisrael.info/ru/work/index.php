83c1c33cb5a4bc0adc890d048376d3dc6d12aa79
Oleg Sher <github@sher.biz>
2016-10-16 22:50:05 +0300

    rentcarisrael.info work

--- rentcarisrael.info/ru/work/index.php
+++ rentcarisrael.info/ru/work/index.php
@@ -7,11 +7,11 @@
         <?php include("__header.html"); ?>
     </head>
     <body>
-        //<?php include("__facebook.html"); ?>
+        <!--//<?php include("__facebook.html"); ?>
         //<?php include("__google.html"); ?>
         //<?php include("__mailru.html"); ?>
         //<?php include("__yandex.html"); ?>
-        //<?php include("__shema.html"); ?>
+        //<?php include("__shema.html"); ?> -->
 
         <?php include("__top.html"); ?>
         <?php include("_index.html"); ?>
