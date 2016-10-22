90479ea99574c1ae32eda816e47e1f7898a4c0ea
Oleg Sher <github@sher.biz>
2016-10-22 17:08:10 +0300

    rentcarisrael.info work

--- rentcarisrael.online/ru/minivan.php
+++ rentcarisrael.online/ru/minivan.php
@@ -6,6 +6,8 @@
 <meta  name="description"  content="Микроавтобусы и минивэны 7-8-9 мест аренда прокат автомобиля в Израиле"/>
 <meta property="og:title" content="Микроавтобусы и минивэны аренда прокат автомобиля в Израиле от компании Vastama Ltd тел +972-58-7710101" />
 <meta property="og:type" content="website" />
+<meta property="og:image" content="http://www.rentcarisrael.online/images/catZ.jpg"/>
+<meta property="og:site_name" content="RentCarIsrael.online Бронирование Микроавтобусы и минивэны 7-8-9 мест аренда прокат автомобиля в Израиле"/>
 
 <?php include("__header.html"); ?>
 </head>
