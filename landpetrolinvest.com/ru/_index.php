36e54ef3652ce2d05cdb7ff444321138d05b987f
olegsher <github@sher.biz>
2016-10-23 09:44:38 +0300

    rentcarisrael.info

---
+++ landpetrolinvest.com/ru/_index.php
@@ -0,0 +1,8 @@
+<div class="container">
+	<div class="col-md-5 bann-info1 wow fadeInLeft animated" data-wow-delay=".5s">
+<!--		<i class="fa fa-columns"></i>-->
+<i> <img src="images/" alt="" class="img-responsive" /></i>
+		<h3>Under construction - Russian</h3>
+                
+                
+</div>
---
+++ landpetrolinvest.com/ru/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Under Construction - Russian</title>
+<meta name="keywords" content="" />
+<meta  name="description"  content="" />
+<?php include("__header.html"); ?>
+</head>
+<body>
+<?php include("__top.html"); ?>
+</body>
+</html>
+
+
