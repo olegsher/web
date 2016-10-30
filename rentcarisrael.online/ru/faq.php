c3c5461383f5994c7fe517579d23e3b920db44ac
Oleg Sher <github@sher.biz>
2016-10-30 23:25:56 +0200

    rentcarisrael.online

---
+++ rentcarisrael.online/ru/faq.php
@@ -0,0 +1,58 @@
+<!DOCTYPE HTML>
+<html>
+<head>
+<title>Часто задаваемые вопросы аренда прокат автомобиля в Израиле</title>
+<meta name="keywords" content="микроавтобус, минивэн 9 мест, минивэн 8 мест, минивэн 7 мест, машина прокат, машина прокат Израиле, аренда машины Израиль, avto prokat, prokat mashin, auto arenda, прокат аренда машин, где можно взять машину напрокат, заказать машину, аренда иномарок, arenda avto, стоимость аренды машины, израиль прокат автомобилей, съем машины Израиле, аренда авто, прокат машины, сьем автомобиля,автомобиль,  машина Израиле, дешевый прокат автомобиля" />
+<meta  name="description"  content="Микроавтобусы и минивэны 7-8-9 мест аренда прокат автомобиля в Израиле"/>
+<meta property="og:title" content="Микроавтобусы и минивэны аренда прокат автомобиля в Израиле от компании Vastama Ltd тел +972-58-7710101" />
+<meta property="og:image" content="http://www.rentcarisrael.online/images/catZ.jpg"/>
+<meta property="og:site_name" content="RentCarIsrael.online Бронирование Микроавтобусы и минивэны 7-8-9 мест аренда прокат автомобиля в Израиле"/>
+
+
+<?php include("__header.html"); ?>
+</head>
+<body>
+<?php include("__counters.html"); ?>
+<?php include("__top.html"); ?>
+
+    
+    
+<div  class="container">
+      
+        
+      <div  class="col-md-7 bann-info wow fadeInRight animated"  data-wow-delay=".5s">
+        <img  src="images/catZ.jpg"  alt="минивэн аренда прокат, минивэн прокат Израиле, аренда минивэны Израиль"
+
+           class="img-responsive"> <br>
+        <h1>Часто задаваемые вопросы по аренде прокату автомобиля в Израиле</h1>
+        <p style="color:grey; font-weight: bold;" >Пассажирские микроавтобусы 9 мест и минивэны
+      </div>
+      <h3>Оплата</h3>
+      <br>
+      Мы входим в пятерку ведущих компаний в Израиле по прокату 9 местных машин (минивэн - 8 пассажиров +
+      водитель), у нас эти автомобили представлены категорией Z<a  href="/">(от 131$/в день
+        - Заказать</a>) которая включает в себя такие автомобили как <a  href="" target="_blank">VW
+      Transporter</a>, <a  href="http://citroen.drom.ru/jumpy/" target="_blank">Citroen Jumpy</a>, <a  href="http://hyundai.drom.ru/h1/" target="_blank">Hyundai i800</a> и другие, все автомобили с
+      автоматической коробкой передач, франшиза (личное участие) на данную категорию минивэнов - 960$
+      <p style="color:teal; font-weight: lighter;" >особо выгодное предложение - минивэн 9 мест автомат всего 2120$ за месяц аренды(обязательные страховки включены в цену)
+      <div  class="clearfix"></div>    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+<?php include("__footer.html"); ?>
+</body>
+</html>
