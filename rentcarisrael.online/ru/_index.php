61f2170ff6951ed40942580bfb0c1f6ce8ae2d9f
Oleg Sher <github@sher.biz>
2016-11-02 00:06:36 +0200

    rentcarisrael.online

--- rentcarisrael.online/ru/_index.php
+++ rentcarisrael.online/ru/_index.php
@@ -5,15 +5,11 @@
 		<h3>БРОНИРОВАНИЕ АВТОМОБИЛЯ В ИЗРАИЛЕ ЗА ОДИН ШАГ</h3>
                 
                     
-                <hr />
-                <a style="float:left; color:grey; font-weight: bold; font-size: large"  href="http://www.rentcarisrael.online/minivan.php" Прокат минивэнов - наш бизнес </a>
-                <hr />
-                <p style="float:left; color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/tours.php">Акция для клиентов - Частный гид за 250$</a> </p>
-                <hr />
-                <p style="float:left; color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/transfer.php">Частный трансфер из аэропорта Бен Гурион начиная от 60$</a> </p>
+                <hr /><p style="float:left; color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/minivan.php">Прокат минивэнов - наш бизнес</a> </p>
+                <hr /><p style="float:left; color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/tours.php">Акция для клиентов - Частный гид за 250$</a> </p>
+                <hr /><p style="float:left; color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/transfer.php">Частный трансфер из аэропорта Бен Гурион начиная от 60$</a> </p>
                 
-                <hr />
-                <p style="float:left; color:grey; font-weight: bold; font-size: large" >Отделения во всех крупных городах Израиля </p>
+                <hr /><p style="float:left; color:grey; font-weight: bold; font-size: large" >Отделения во всех крупных городах Израиля </p>
                 <hr />
                 <p style="float:left; color:grey; font-weight: bold; font-size: large" >Неограниченный километраж при заказе 3-30 дней </p>
                 <hr />
