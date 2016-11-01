e2c3017322a31a04d488bcf6104693b6380cabf3
Oleg Sher <github@sher.biz>
2016-11-01 23:54:15 +0200

    rentcarisrael.online

--- rentcarisrael.online/ru/_index.php
+++ rentcarisrael.online/ru/_index.php
@@ -3,7 +3,8 @@
 <!--		<i class="fa fa-columns"></i>-->
 <i> <img src="images/Thrifty-Israel.jpg" alt="машина прокат, машина прокат Израиле, аренда машины Израиль" class="img-responsive" /></i>
 		<h3>БРОНИРОВАНИЕ АВТОМОБИЛЯ В ИЗРАИЛЕ ЗА ОДИН ШАГ</h3>
-                
+            </div>
+<div>    
                 <hr style="float:left; color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/minivan.php">Прокат минивэнов - наш бизнес</a> 
                 <hr style="color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/tours.php">Акция для клиентов - Частный гид за 250$</a>
                 <hr style="color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/transfer.php">Частный трансфер из аэропорта Бен Гурион начиная от 60$</a>
