5cab6d983d3395010d74359a97914f5143dbecfd
Oleg Sher <github@sher.biz>
2016-11-01 23:57:26 +0200

    rentcarisrael.online

--- rentcarisrael.online/ru/_index.php
+++ rentcarisrael.online/ru/_index.php
@@ -3,15 +3,16 @@
 <!--		<i class="fa fa-columns"></i>-->
 <i> <img src="images/Thrifty-Israel.jpg" alt="машина прокат, машина прокат Израиле, аренда машины Израиль" class="img-responsive" /></i>
 		<h3>БРОНИРОВАНИЕ АВТОМОБИЛЯ В ИЗРАИЛЕ ЗА ОДИН ШАГ</h3>
-                <div>
-                <hr style="float:left; color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/minivan.php">Прокат минивэнов - наш бизнес</a> 
-                <hr style="color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/tours.php">Акция для клиентов - Частный гид за 250$</a>
-                <hr style="color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/transfer.php">Частный трансфер из аэропорта Бен Гурион начиная от 60$</a>
+                
+                    <hr />
+                <p style="float:left; color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/minivan.php">Прокат минивэнов - наш бизнес</a> 
+                <p style="color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/tours.php">Акция для клиентов - Частный гид за 250$</a>
+                <p style="color:grey; font-weight: bold; font-size: large" ><a  href="http://www.rentcarisrael.online/transfer.php">Частный трансфер из аэропорта Бен Гурион начиная от 60$</a>
                 
                 <hr style="color:grey; font-weight: bold; font-size: large" >Отделения во всех крупных городах Израиля 
                 <hr style="color:grey; font-weight: bold; font-size: large" >Неограниченный километраж при заказе 3-30 дней
                 <hr style="color:grey; font-weight: bold; font-size: large" >Огромные скидки при заказе 30+ дней               
-            </div>    
+              
                 
                 
 	</div>
