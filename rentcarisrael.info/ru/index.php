dbca3eabde1dc30466f3c98d557868416f9b172d
olegsher <github@sher.biz>
2016-11-03 15:14:41 +0200

    rentcarisrael.info

--- rentcarisrael.info/ru/index.php
+++ rentcarisrael.info/ru/index.php
@@ -1,22 +1,16 @@
-<!DOCTYPE HTML>
-<html lang="ru">
-<head>
-<title>Недорогая аренда прокат автомобиля в Израиле | RentCarIsrael.info</title>
-<meta name="keywords" content="машина прокат, машина прокат Израиле, аренда машины Израиль, avto prokat, prokat mashin, auto arenda, прокат аренда машин, где можно взять машину напрокат, заказать машину, аренда иномарок, arenda avto, стоимость аренды машины, израиль прокат автомобилей, съем машины Израиле, аренда авто, прокат машины, сьем автомобиля,автомобиль,  машина Израиле, дешевый прокат автомобиля" />
-<meta  name="description"  content="аренда прокат автомобиля в Израиле, эконом класс и семейные машины, микроавтобусы и минивэны 7-8-9 мест" />
-    <?php include("__header.html"); ?>
-</head>
-<body>
-<?php include("__facebook.html"); ?>
-<?php include("__google.html"); ?>
-<?php include("__mailru.html"); ?>
-<?php include("__yandex.html"); ?>
-<?php include("__shema.html"); ?>
-
-<?php include("__top.html"); ?>
-<?php include("_index.php"); ?>
-<?php include("__footer.html"); ?>
-</body>
+<!DOCTYPE html>
+<!--
+To change this license header, choose License Headers in Project Properties.
+To change this template file, choose Tools | Templates
+and open the template in the editor.
+-->
+<html>
+    <head>
+        <title>Rentcarisrael.info Russian Under construction</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    </head>
+    <body>
+        <div>Russian Under construction</div>
+    </body>
 </html>
-
-
