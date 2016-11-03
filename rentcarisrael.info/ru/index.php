8b90db4c3f8b520aa7c4f50156521072c8f84019
olegsher <github@sher.biz>
2016-11-03 13:06:06 +0200

    rentcarisrael.info

---
+++ rentcarisrael.info/ru/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE HTML>
+<html lang="ru">
+<head>
+<title>Недорогая аренда прокат автомобиля в Израиле | RentCarIsrael.info</title>
+<meta name="keywords" content="машина прокат, машина прокат Израиле, аренда машины Израиль, avto prokat, prokat mashin, auto arenda, прокат аренда машин, где можно взять машину напрокат, заказать машину, аренда иномарок, arenda avto, стоимость аренды машины, израиль прокат автомобилей, съем машины Израиле, аренда авто, прокат машины, сьем автомобиля,автомобиль,  машина Израиле, дешевый прокат автомобиля" />
+<meta  name="description"  content="аренда прокат автомобиля в Израиле, эконом класс и семейные машины, микроавтобусы и минивэны 7-8-9 мест" />
+    <?php include("__header.html"); ?>
+</head>
+<body>
+<?php include("__facebook.html"); ?>
+<?php include("__google.html"); ?>
+<?php include("__mailru.html"); ?>
+<?php include("__yandex.html"); ?>
+<?php include("__shema.html"); ?>
+
+<?php include("__top.html"); ?>
+<?php include("_index.php"); ?>
+<?php include("__footer.html"); ?>
+</body>
+</html>
+
+
