c1593960ff96df38627ca9bbb4e836ab36474ae7
olegsher <github@sher.biz>
2016-11-03 15:52:58 +0200

    rentcarisrael.info

--- rentcarisrael.info/en/index.php
+++ rentcarisrael.info/en/index.php
@@ -1,16 +1,27 @@
-<!DOCTYPE html>
-<!--
-To change this license header, choose License Headers in Project Properties.
-To change this template file, choose Tools | Templates
-and open the template in the editor.
--->
-<html>
-    <head>
-        <title>Rentcarisrael.info English Under construction</title>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    </head>
-    <body>
-        <div>English Under construction</div>
-    </body>
+<!DOCTYPE HTML>
+<html lang="en">
+<head>
+<title>Rental car in Israel | RentCarIsrael.info</title>
+<meta name="keywords" content="" />
+<meta  name="description"  content="" />
+    <?php include("__header.html"); ?>
+</head>
+<body>
+<?php include("__shema.html"); ?>
+<div class="container">
+	<div class="col-md-5 bann-info1 wow fadeInLeft animated" data-wow-delay=".5s">
+<!--		<i class="fa fa-columns"></i>-->
+<i> <img src="../images/Thrifty-Israel.jpg" alt="" class="img-responsive" /></i>
+		<h3>Under construction</h3>
+</div>
+<hr />
+    <p style="color:grey; font-weight: bold; font-size: large" >Please visit us soon </p>
+
+
+
+</div>
+
+
+<?php include("__footer.html"); ?>
+</body>
 </html>
